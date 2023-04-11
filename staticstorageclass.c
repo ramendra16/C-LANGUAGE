c29845ab0ea31210b80d583a18b0f1259a50f47f
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2023-04-11 22:05:59 +0530

    C FILE

---
+++ staticstorageclass.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void function();
+int main()
+{
+	function();
+	function();
+	function();
+	return 0;
+}
+void function()
+{
+	int a=10;
+	static int b=10;
+	printf("\n value of a =%d, Value of b=%d", a,b);
+	a++;
+	b++;
+}
