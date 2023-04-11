4018c7115f2c2bbe5391c386dca08e51c79143b4
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2023-04-11 22:03:50 +0530

    C FILE

---
+++ tableprog2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	do{
+	
+	printf("enter the num to print the table: ");
+	scanf("%d", &n);
+	
+	print("enter the value of multiplicative number: ");
+	scanf("%d", &i);
+	}
+	
+	
+	while(i<=10);
+		{
+		printf("%d * %d = %d\n", n,i,(n*i));
+		i++;
+	}
+	return 0;
+}
+
