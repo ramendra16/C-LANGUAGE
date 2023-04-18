7c16e2091381b902ceea8c441894fe83e4195b76
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2023-04-18 23:22:45 +0530

    C FILE

---
+++ passing array to func using by value.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void value(int);
+int main()
+{
+	int arr[100],n;
+	int i;
+	printf("\n Enter n:");
+	scanf("%d", &n);
+	printf("\n Enter array elements:");
+	for (i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	printf("\nPassing Elements by value:");
+	for(i=0;i<n;i++)
+	{
+		value(arr[i]);
+	}
+	return 0;
+} 
+	
+	void value(int u)
+	{
+	printf("%d ", u);
+	}
